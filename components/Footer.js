7387f13c0af36d75a0375f06beeac28cfa508784
sarac <saracurak99@gmail.com>
2023-08-25 23:37:48 +0200

    layout

---
+++ components/Footer.js
@@ -0,0 +1,60 @@
+import { useState, useEffect } from 'react';
+import Image from 'next/image';
+import logoSrc from '../public/images/logo.png'
+import facebook from '../public/images/facebook.png'
+import instagram from '../public/images/instagram.png'
+
+
+const Footer = () => {
+    const [isClicked, setIsClicked] = useState(false);
+
+    useEffect(() => {
+    }, []);
+
+    return (
+        <div className="footer">
+            <hr className="footer-upper-hr"/>
+            <div className="footer-menu">
+                <div className="footer-menu-left">
+                    <div className="footer-menu-container">
+                        <span><a href="#"> home </a></span>
+                        <span><a href="#"> shop  </a></span>
+                        <span><a href="#"> account </a></span>
+                    </div>
+                </div>
+                <div className="footer-logo">
+                    <a href="/">
+                        <img src={logoSrc}/>
+                    </a>
+                </div>
+                <div className="footer-menu-right">
+                    <div className="footer-menu-container">
+                        <span><a href="#"> faq </a></span>
+                        <span><a href="#"> privacy policy </a></span>
+                        <span><a href="#"> terms & conditions </a></span>
+                    </div>
+                </div>
+            </div>
+            <hr/>
+            <div className="footer-bottom">
+                <div className="footer-social">
+                    <div className="footer-social-item">
+                        <a href="#">
+                            <img src={facebook}/>
+                        </a>
+                    </div>   
+                    <div className="footer-social-item">
+                        <a href="#">
+                            <img src={instagram}/>
+                        </a>
+                    </div>   
+                </div>
+                <div className="footer-copyright">
+                    &copy; inSTrukcije 2023
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Footer;
---
+++ components/Navbar.js
@@ -0,0 +1,99 @@
+import React from 'react'
+import logoSrc from '../public/images/logo.png'
+import { useState } from 'react'
+import { useEffect } from 'react';
+import Image from 'next/image';
+import {useRouter} from 'next/router';
+
+const menuItemsList = [
+  {
+      name: 'home',
+      slug: '',
+      isClicked: false
+  },
+  {
+      name: 'Instruktori',
+      slug: 'instruktori',
+      isClicked: false
+  },
+  {
+    name: 'Predmeti',
+    slug: 'predmeti',
+    isClicked: false
+  },
+  {
+    name: 'Uputstva',
+    slug: 'uputstva',
+    isClicked: false
+  },
+  {
+    name: 'Cjenik',
+    slug: 'cjenik',
+    isClicked: false
+  },
+  {
+      name: 'FAQ',
+      slug: 'faq',
+      isClicked: false
+  },
+  {
+      name: 'Kontakt',
+      slug: 'kontakt',
+      isClicked: false
+  }
+];
+
+const Navbar = () => {
+  const [menuItems, setMenuItems] = useState(menuItemsList);
+  const [isClicked, setIsClicked] = useState(false);
+  const [test123, setTest123] = useState([]);
+  const router = useRouter()
+  
+  useEffect(() => {
+
+    console.log(logoSrc)
+
+    let updatedMenuItems = menuItemsList.map(item => {
+        if(item.slug == router.pathname.split('/').pop()) item.isClicked = true;
+        return item;
+    })
+
+    setMenuItems(updatedMenuItems);
+    setIsClicked(true);
+  }, [test123]);
+
+  return (
+    <header>
+    <nav className="navbar sticky-nav">
+        <div className="box navbar-content">
+            <div className="logo">
+                <a href="/">
+                    <img src={logoSrc.src}/>
+                </a>
+            </div>
+            <ul className="menu-items">
+                        {menuItems.map((item) => (
+                            <a href={item.slug} key={item.slug + item.name}><li className={`menu-item ${item.isClicked ? "menu-item-clicked" : ""}`}>{item.name}</li></a>
+                        ))}
+            </ul>
+            <div className="buttons-container">
+                <a href="/account">
+                    <button className="button-empty">
+                        <span> Prijavi se </span>
+                    </button>
+                </a>
+
+                <a href="/shop">
+                    <button className="button-full">
+                        <span> Registriraj se </span>
+                    </button>
+                </a>
+            </div>
+          </div>
+    </nav>
+    </header>
+
+  )
+}
+
+export default Navbar
