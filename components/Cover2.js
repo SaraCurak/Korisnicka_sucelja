09739691203c74f74ed977116645af21034cb5dc
sarac <saracurak99@gmail.com>
2023-09-05 13:25:52 +0200

    Cover icons

--- components/Cover2.js
+++ components/Cover2.js
@@ -1,9 +1,8 @@
 import React from 'react';
-import icon1 from '../public/images/icon1.png'
-import icon2 from '../public/images/icon2.png'
-import icon3 from '../public/images/icon3.png'
-import icon4 from '../public/images/icon4.png'
-
+import icon1 from '../public/images/icon1.png';
+import icon2 from '../public/images/icon2.png';
+import icon3 from '../public/images/icon3.png';
+import icon4 from '../public/images/icon4.png';
 
 const Cover2 = () => {
   return (
@@ -11,45 +10,32 @@ const Cover2 = () => {
       <div className="brushed-title-container-cover">
         <h2 className="brush">Instrukcije 1 na 1</h2>
       </div>
-      <div className="cover2-content">
-        <div className="cover2-text">
-          <p>
+      <div className="cover2-text">
+        <p>
           Uz individualni pristup naši učenici stječu samopouzdanje, pospješuju vještine i poboljšavaju ocjene.
-          </p>
-          <p>
+        </p>
+        <p>
           Prije susreta, naši instruktori se uvijek pripreme temeljem tvojih napomena. Također, uvijek ti je dostupan isti instruktor za online instrukcije koji prati tvoj napredak i upoznat je s tvojim potrebama, bez obzira da li pohađaš osnovnu školu, srednju školu ili fakultet.
-          </p>
-
+        </p>
+      </div>
+      <div className="icon-container">
+        <div className="icon">
+          <img src={icon1.src} alt="Icon 1" />
+          <h3>Popravljanje ocjena</h3>
         </div>
-        <div className="icon-container">
-          {/* Add your icons here */}
-          <div className="icon">
-            <img src={icon1.src} alt="Icon 1" />
-            <h3>Popravljanje ocjena</h3>
-         
-          </div>
-          <div className="icon">
-          
-            <img src={icon2.src} alt="Icon 2" />
-            <h3>Priprema mature</h3>
-            
-          </div>
-          <div className="icon">
-        
-            <img src={icon3.src} alt="Icon 3" />
-            <h3>Pomoć pri učenju</h3>
-        
-          </div>
-          <div className="icon">
-            <img src={icon4.src} alt="Icon 4" />
-            <h3>Napredni program</h3>
-          
-          </div>
+        <div className="icon">
+          <img src={icon2.src} alt="Icon 2" />
+          <h3>Priprema mature</h3>
+        </div>
+        <div className="icon">
+          <img src={icon3.src} alt="Icon 3" />
+          <h3>Pomoć pri učenju</h3>
+        </div>
+        <div className="icon">
+          <img src={icon4.src} alt="Icon 4" />
+          <h3>Napredni program</h3>
         </div>
       </div>
-      <p>
-          Prije susreta, naši instruktori se uvijek pripreme temeljem tvojih napomena. Također, uvijek ti je dostupan isti instruktor za online instrukcije koji prati tvoj napredak i upoznat je s tvojim potrebama, bez obzira da li pohađaš osnovnu školu, srednju školu ili fakultet.
-          </p>
     </div>
   );
 };
