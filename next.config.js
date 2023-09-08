aa0ed74e46a5d130600ae539c12c4dc4620fb085
sarac <saracurak99@gmail.com>
2023-09-08 10:13:07 +0200

    verce6

--- next.config.js
+++ next.config.js
@@ -1,15 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
+  reactStrictMode: true, 
+  typescript: { ignoreBuildErrors: true }
 }
 
 module.exports = nextConfig
-module.exports = {
-  typescript: {
-    // !! WARN !!
-    // Dangerously allow production builds to successfully complete even if
-    // your project has type errors.
-    // !! WARN !!
-    ignoreBuildErrors: true,
-  },
-}
