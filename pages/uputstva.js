52aa2f6ac1fa86dc6801d677020fa370d6df19ac
sarac <saracurak99@gmail.com>
2023-09-08 05:39:19 +0200

    vercel2

--- pages/uputstva.js
+++ pages/uputstva.js
@@ -1,13 +1,13 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import { Inter } from '@next/font/google'
-import styles from '@/styles/Home.module.css'
+
 
 import Navbar from '../components/Navbar'
 import Sidebar from '../components/Sidebar'
 import Navbar2 from '../components/Navbar2'
-import Footer from '@/components/Footer'
-import Footer2 from '@/components/Footer2'
+import Footer from '../components/Footer'
+import Footer2 from '../components/Footer2'
 import Cover from '../components/Cover'
 import Steps from '../components/Steps'
 
